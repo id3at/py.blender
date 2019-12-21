17bdfe983c0ddf18da0b281068b749c406f97e22
id3at <58143208+id3at@users.noreply.github.com>
2019-12-21 16:06:48 +0100

    Update data.py

--- data.py
+++ data.py
@@ -6,10 +6,9 @@ który zmienia się wraz z klatkami animacji
 import bpy
 import datetime
 g = datetime.datetime.today()
+bpy.ops.object.text_add(enter_editmode=False, location=(0, 0, 0))
 
-def u(g):
-    bpy.ops.object.delete(use_global=False)
-    bpy.ops.object.text_add(enter_editmode=False, location=(0, 0, 0))
+def u(g): 
     f = bpy.context.scene.objects['Text']
     g = datetime.datetime.today()
     f.data.body = f'{g}'
